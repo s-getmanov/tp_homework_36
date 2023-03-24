be34dff1e63dd185e53f67b12dc00138b6d0c8f3
Sergey <you@example.com>
2023-03-24 09:27:10 +0300

    Добавлены основные файлы по задаче

---
+++ main1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+    
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("5 x %i = %i\n",i, 5*i);
+    }    
+}
---
+++ main2.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+main()
+{
+
+    int exit_number = 7;
+    int a = 0;
+
+    while (a != exit_number)
+    {
+        printf("Введите число: ");
+        scanf("%i", &a);
+
+        if (a != exit_number)
+        {
+            if (a > exit_number)
+            {
+                printf("Больше %i!\n", exit_number);
+            }
+            else
+            {
+                printf("Меньше %i!\n", exit_number);
+            };
+
+            if (a > 10)
+            {
+                printf("Больше 10!\n");
+            }
+            else if (a < 10)
+            {
+                printf("Меньше 10\n");
+            };
+
+            if (a % 2 > 0)
+            {
+                printf("Не делится на 2\n");
+            }
+            else
+            {
+                printf("Делится на 2\n");
+            };
+
+            if (a % 3 > 0)
+            {
+                printf("Не делится на 3\n");
+            }
+            else
+            {
+                printf("Делится на 3\n");
+            };
+        };
+        
+        printf("\n");
+    }
+}
